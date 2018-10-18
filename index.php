e36a39396537373007a2f1e0c22e51a0a4c785fb
gustavo <gustavopetruzzi19@gmail.com>
2018-10-18 08:03:35 -0300

    anda para cargar cuentas

--- index.php
+++ index.php
@@ -5,7 +5,15 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 require 'clases/cuentaAlta.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+$config['displayErrorDetails'] = true;
 $app = new \Slim\App;
+$app->add(function($request, $response, $next){
+    $response = $next($request, $response);
+    return $response
+         ->withHeader('Access-Control-Allow-Origin', '*')
+         ->withHeader('Access-Control-Allow-Headers', 'X-API-KEY,Access-Control-Request-Method,X-Requested-With, Content-Type, Accept, Origin, Authorization')
+         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+ });
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     
@@ -14,6 +22,7 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $response;
 });
 $app->post('/generarAltaCuenta', function (Request $request, Response $response, array $args) {
+    
     $arrayCuentas = array();
     $arrayErrores = array();
     $nombreArchivo = $_FILES['archivo']['tmp_name'];
@@ -24,10 +33,10 @@ $app->post('/generarAltaCuenta', function (Request $request, Response $response,
     $fecha = new DateTime();
     $encodeado = mb_convert_encoding(" ", "UTF-16LE");
     //$encabezado = "1C16884A" . str_repeat($encodeado, 76);
-    
+    $encabezado = str_pad("1C16884A", 160);
     $gestor = fopen("cuentas-". $fecha->getTimestamp(). ".txt", "w");
-    //fwrite($gestor, $encabezado);
-    //fwrite($gestor, "\r\n");
+    fwrite($gestor, $encabezado);
+    fwrite($gestor, "\r\n");
     for ($fila= 1; $fila < $maxFila +1; $fila++) { 
         //columna 1 cbu, 2 denominacion, 3 cuil 
         try{
@@ -35,14 +44,11 @@ $app->post('/generarAltaCuenta', function (Request $request, Response $response,
             $cuil = $hoja->getCellByColumnAndRow(3, $fila)->getValue();
             $cbu = preg_replace("/[^0-9]/", "", $hoja->getCellByColumnAndRow(1, $fila)->getFormattedValue());
             //$cbu = $hoja->getCellByColumnAndRow(1, $fila)->getValue();
-            /*
-            $tipo = $hoja->getCellByColumnAndRow(7, $fila)->getValue();
-            $tipo = $tipo . $hoja->getCellByColumnAndRow(8, $fila)->getValue();
-            $tipo = $tipo . $hoja->getCellByColumnAndRow(9, $fila)->getValue();
-            */
+            
             $cuenta = new cuentaAlta($cbu, $denominacion, $cuil);
+            array_push($arrayCuentas, $cuenta);
             $algo = "ó";
-            array_push($arrayCuentas, strlen($algo));
+            //array_push($arrayErrores, $cuil);
             //array_push($arrayCuentas, $denominacion);
             //array_push($arrayCuentas, $cbu);
             //array_push($arrayCuentas, $cuenta->cuit);
@@ -51,17 +57,43 @@ $app->post('/generarAltaCuenta', function (Request $request, Response $response,
 
         }
         catch(Exception $e){
-            array_push($arrayErrores, $fila);
+            $linea = array($cbu, $denominacion, $cuil, $e->getMessage());
+            array_push($arrayErrores, $linea);
         }
     }
     
     //fwrite($gestor, "3C16884A" . str_repeat("0", 6- count($arrayCuentas)) . count($arrayCuentas) . str_repeat($encodeado, 73));
-    
+    if(count($arrayCuentas) > 99){
+        $finalCliente = "3C16884A" . "000" . count($arrayCuentas);
+        $final = str_pad($finalCliente, 160);
+        fwrite($gestor, $final);
+    }
+    else if(count($arrayCuentas) > 9){
+        $finalCliente = "3C16884A" . "0000" . count($arrayCuentas);
+        $final = str_pad($finalCliente, 160);
+        fwrite($gestor, $final);
+    }
     fclose($gestor);
     $retorno['cuentas'] = $arrayCuentas;
     $retorno['errores'] = $arrayErrores;
+    $retorno['link']= "algo";
     return $response->withJson($retorno);
     
+    //return $response->withJson($_FILES);
+});
+
+$app->post('/generarTransferenciasSueldos', function (Request $request, Response $response, array $args){
+    $arrayCuentas = array();
+    $arrayErrores = array();
+    $nombreArchivo = $_FILES['archivo']['tmp_name'];
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+    $archivo = $reader->load($nombreArchivo);
+    $hoja = $archivo->getActiveSheet();
+    $maxFila = $hoja->getHighestRow();
+    $fecha = new DateTime();
+
+    $gestor = fopen("transferencias-" . $fecha->getTimestamp() . ".txt", "w");
+    $encabezado = "*U*1111111111211111111112D";
 });
 $app->run();
 
